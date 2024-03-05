4792ea34b3149726d1618c204a58c144ec1ba5c8
Sarvesh Dwivedi <sarvesh.dwivedi@reddoorz.com>
2024-03-05 16:47:22 +0530

    add auth with user sign in lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_103158) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_111605) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -37,8 +37,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_103158) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "locked_at"
+    t.string "unlock_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "comments", "articles"
